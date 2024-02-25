efb6a7d088c0400cad6028563a2d8a65b8e5ac29
WDRshadow <i@wdr.im>
2024-02-25 14:03:38 +0100

    fix: fit to server

---
+++ src/client/java/com/jackdaw/chatwithnpc/ChatWithNPCModClient.java
@@ -0,0 +1,9 @@
+package com.jackdaw.chatwithnpc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChatWithNPCModClient implements ClientModInitializer {
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
